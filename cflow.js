5f5f5e6b7728134f5e1410657fc0acb1dfa744c4
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-22 14:28:57 -0500

    initial commit

---
+++ cflow.js
@@ -0,0 +1,12 @@
+//PART1: Growing Pains
+
+const areaRadius = 5;
+const PI = 3.1415;
+
+const area = PI * areaRadius **2;
+const minimumSpacePlant = 0.8;
+let numberOfPlants = 20;
+
+let numberOfWeeks;
+
+
