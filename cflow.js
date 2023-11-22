09faca3c84e2a99ed3a8beabd682114f17dde647
josecobi <58313777+josecobi@users.noreply.github.com>
2023-11-22 16:04:38 -0500

    created conditions to make decisions

--- cflow.js
+++ cflow.js
@@ -1,12 +1,28 @@
-//PART1: Growing Pains
-
+// >>>>>>>>>>>>>>>>>>>>>>>>PART1: Growing Pains<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+//Case1: Number of weeks is 1
 const areaRadius = 5;
 const PI = 3.1415;
-
+// Calculate the area
 const area = PI * areaRadius **2;
-const minimumSpacePlant = 0.8;
+let numberOfWeeks = 1;
+
+// Calculate maximum number of plants that can fit into that area
+const minSpacePlant = 0.8;
+const maxNumPlantsInArea = area / minSpacePlant;
 let numberOfPlants = 20;
 
-let numberOfWeeks;
+// Create conditions in order to make decisions and display them
+if(numberOfPlants > maxNumPlantsInArea * 0.8){
+    console.log("Pruned");
+}
+else if(numberOfPlants >= maxNumPlantsInArea * 0.5 && numberOfPlants <= maxNumPlantsInArea * 0.8) {
+    console.log("Monitored");
+}
+else {
+    console.log("Planted");
+}
 
 
+//Part 2: 
+// Calculate the area taken by the plants
+let areaTakenByPLants = minSpacePlant * numberOfPlants;
